f61d92708e1f772cc079c4bcd3514259104aa699
Raden-nvii <muhammadnavi01@mail.ugm.com>
2022-06-11 09:12:59 +0700

    endpoint change local

--- app.js
+++ app.js
@@ -18,22 +18,28 @@ sequelize.authenticate().then(() =>{
 app.post("/hasil", async (req, res) => {
     try {
         
-        const {input} = req.body;
-        console.log(input);
+        const {text} = req.body;
 
-        const response = await fetch('https://classifiermodel-k5eyux7eqa-et.a.run.app/descam/predict', {
+        const response = await fetch('http://127.0.0.1:8080/descam/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8'},
-            body: JSON.stringify({input:input}),
+            body: JSON.stringify({input:text}),
         })
-
+        console.log(response);
+        const results = response.json({});
+        // const Out = response.output;
+        // const Scc = response.success;
+        // console.log(Out);
+        // console.log(Scc);
         return res.json(await response.json());
     } 
     catch (err) {
         console.error(err.message);
         res.status(500).send("server error");
     }
+
+    // console.log(text);
 });
 
 const history_route = require('./routes/history_route');
